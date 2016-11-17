fec9b7d98d0f12774c12c0f598a7e4462a5f8aaa
longrw <longrw@foxmail.com>
2016-11-17 15:34:00 +0800

    wrong????

--- runPipeline__.py
+++ runPipeline__.py
@@ -32,7 +32,7 @@ def multi_process(func, args, np=""):
 
 
 def bcl2fastq(*argv, **kw):
-    samplesheet = os.path.join(files, "SampleSheet.csv")
+    #samplesheet = os.path.join(files, "SampleSheet.csv")
     if 'NS500713' in rawseq:
         os.system("bcl2fastq_v2_18 -r 72 -d 72 -p 72 -w 72 -R %s --sample-sheet %s -o %s --barcode-mismatches=0 --no-lane-splitting" % (rawseq, samplesheet, rawfq))
         logging.info("\nBcl2fastq_v2_18 CMD: bcl2fastq -r 72 -d 72 -p 72 -w 72 -R %s --sample-sheet %s -o %s --barcode-mismatches=0 --no-lane-splitting\n%s" % (rawseq, samplesheet, rawfq, "#"*50))
@@ -210,7 +210,8 @@ def main():
     # bcl2fastq
     lane_s = options.lane_start
     lane_e = options.lane_end
-    bcl2fastq(rawseq, files, rawfq, lane_s, lane_e)
+    samplesheet = os.path.join(files, "SampleSheet.csv")
+    bcl2fastq(rawseq, samplesheet, rawfq, lane_s, lane_e)
     index = os.path.join(files, "fqsamplesheet.csv")
     fqsplit(index, rawfq, rawfq)
     # filter files and mkdir new folders
